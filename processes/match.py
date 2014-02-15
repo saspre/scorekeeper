5fff7d01cd89b2573e07908b0762929876afa5d2
rprentow@gmail.com <rpentow@gmail.com>
2014-02-15 21:35:03 +0100

    Added processes and stuff

---
+++ processes/match.py
@@ -0,0 +1,10 @@
+#match.py
+
+#Proccess for management of current match
+
+import threading
+
+class Match (threading.Thread)
+
+	def run(self):
+		print('Match Started')
--- scorekeeper.py
+++ scorekeeper.py
@@ -1,1 +1,10 @@
 #main launcher
+
+
+#import model.matches;
+
+from model.matches import Match
+
+match = Match();
+
+print(match.match_pk)
