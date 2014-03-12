f6d29e38901a74b187d863db3dca8e8d612b8d0c
alex <abondoa@gmail.com>
2014-03-12 18:32:18 +0100

    rmed old model files. Might be added later if the models.py file grows too large - I don't quite get the python package/namespace convention yet

--- models.py
+++ models.py
@@ -5,9 +5,10 @@
 from sqlalchemy import create_engine, Column, Integer, String, Sequence, Table, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, backref
-from model.base import Base
+from sqlalchemy.ext.declarative import declarative_base
 import config
 
+Base = declarative_base()
 engine = create_engine(config.configSectionMap('database')['name'],echo=False)
 Session = sessionmaker(bind=engine)
 
