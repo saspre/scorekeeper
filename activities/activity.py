7de9fd69e4aec59d1eb2a6cfc3cb07f309b9e041
Rasmus Prentow <rprentow@gmail.com>
2014-03-16 16:41:23 +0100

    Removed Circular dependency

--- activities/activity.py
+++ activities/activity.py
@@ -1,10 +1,8 @@
 import zmq
-from processes.controller import ControllerProcess
 
 class Activity:
     
     def __init__(self, controller):
-        assert controller is ControllerProcess, "Controller is not the ControllerProcess"
         self.controller = controller
         
     
